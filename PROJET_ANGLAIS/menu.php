3722e65a7b56a8de0ae3b8d4b3c4ecac73c92277
Victoria Martin <victoria.martin@telecomnancy.net>
2017-03-18 18:59:19 +0100

    Cours faias

---
+++ PROJET_ANGLAIS/menu.php
@@ -0,0 +1,35 @@
+<?php
+<div class="header-wrap">
+  <div class="logo">
+    <h1>Valide ton semestre à TNCY !</h1>
+  </div>
+  <div class="menu"> <img src="images/menu-left.png" alt="image"/>
+      <ul class="sf-menu" id="example">
+        <li><a href="index.html">Accueil</a></li>
+        <li class="current"> <a href="#">Entraine toi ! </a>
+          <ul>
+            <li> <a href="#">1ère année</a> 
+               <ul>
+                <li> <a href="works.html">Cours </a> </li>
+                <li><a href="exercices1A.php">Exercices</a></li>
+              </ul>
+            </li>
+            <li class="current"> <a href="#">2ème année</a>
+              <ul>
+                <li> <a href="works.html">Cours </a> </li>
+                <li><a href="exercices2A.php">Exercices</a></li>
+              </ul>
+            </li>
+            <li> <a href="#">3ème année</a>
+               <ul>
+                 <li> <a href="works.html">Cours </a> </li>
+                 <li><a href="exercices3A.php">Exercices</a></li> 
+             </ul>
+          </li>
+          </ul>
+        </li>
+        <li><a href="about.html">A propos</a></li>
+      </ul>
+    <img src="images/menu-right.png" alt="image"/> </div>
+</div>
+?>
